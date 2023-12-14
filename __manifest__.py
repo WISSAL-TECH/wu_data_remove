58d6e4f1c34d745c31cabf28c3d5f3b7c6ceae7e
messai-oumaima <110818130+messai-oumaima@users.noreply.github.com>
2023-12-14 11:37:32 +0100

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo 15 Remove Data',
+    'version': '15.0.1.0.0',
+    'category': 'Tools',
+    'description': 'Data Clean up, Remove Data, Database Clean UP, Reset Database',
+    'summary': 'Data Clean up, Remove Data, Database Clean UP, Reset Database',
+    'author': 'WAMANU',
+    'maintainer': 'WAMANU',
+    'website': 'http://www.wamanu.com',
+    'sequence': 20,
+    'summary': """""",
+    'depends': ['base',],
+    'data': [
+        'views/view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': ['static/description/banner.png'],
+}
